d2188e039751d43fdedf6a820e829f54f377b7d7
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-01-09 02:02:53 +0530

    add new mongodb alert url

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,19 +5,19 @@ import { isAdmin } from "../middleware/isAdmin";
 import { activateUser, suspendUser } from "../controller/user.controller";
 import { upload } from "../middleware/upload";
 
-const routes = Router();
+const router = Router();
 
-routes.post("/register", registerUser);
-routes.post("/login", loginUser);
-routes.post("/google_login", googleLogin)
-routes.post("/forgot-password", forgotPassword);
-routes.post("/reset-password/:token", resetPassword);
-routes.post("/refresh", handleRefreshToken)
-routes.get("/getMyDetails", authenticate, getMyDetails)
-routes.get("/all_users", authenticate, isAdmin, getAllUsers);
-routes.patch("/suspend_user/:id", authenticate, isAdmin, suspendUser);
-routes.patch("/activate_user/:id", authenticate, isAdmin, activateUser);
-routes.patch("/update_profile", authenticate, upload.single("image"), updateProfile);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/google_login", googleLogin)
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+router.post("/refresh", handleRefreshToken)
+router.get("/getMyDetails", authenticate, getMyDetails)
+router.get("/all_users", authenticate, isAdmin, getAllUsers);
+router.patch("/suspend_user/:id", authenticate, isAdmin, suspendUser);
+router.patch("/activate_user/:id", authenticate, isAdmin, activateUser);
+router.patch("/update_profile", authenticate, upload.single("image"), updateProfile);
 
 
-export default routes;
+export default router;
